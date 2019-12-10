b3ba8d0a046630b2ad0658f80151d5474ea18931
Alexander Morozov <sasha.v.morozov@gmail.com>
2019-12-10 21:13:54 +0000

    Cleaning configuration files

--- google_scripts/Platform.js
+++ google_scripts/Platform.js
@@ -1,4 +1,12 @@
 /**
+ * Property objects initialization.
+ *
+ */
+
+var scriptProperties = PropertiesService.getScriptProperties();
+var userProperties = PropertiesService.getUserProperties();
+
+/**
  * The function returns unique identifier of the application script. The Id is used to support development tools.
  *
  * @return {string} unique script identifier
@@ -12,3 +20,4 @@ function getScriptId() {
     Logger.log(ScriptApp.getScriptId());
     return ScriptApp.getScriptId();
 }
+
