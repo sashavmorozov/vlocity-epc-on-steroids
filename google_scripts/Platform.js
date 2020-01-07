45694f8131cd15a3faf1c1b9950d1fd1459e4201
Sasha <sasha.v.morozov@gmail.com>
2020-01-07 18:34:42 +0800

    Tiny documentation improvement for platform.js

--- google_scripts/Platform.js
+++ google_scripts/Platform.js
@@ -5,35 +5,31 @@
  *
  * @example
  *
- *     getScriptId();
+ *     var scriptId = getScriptId();
  */
 
 function getScriptId() {
-    Logger.log(ScriptApp.getScriptId());
+    console.log("*** METHOD_ENTRY: " + arguments.callee.name);
+    console.log(ScriptApp.getScriptId());
+
+    console.log("*** METHOD_EXIT: " + arguments.callee.name);
     return ScriptApp.getScriptId();
 }
 
 /**
  * This function is called on spreadsheet open and merely adds necessary menu items into the taskbar
+ * 
+ * @param 
+ * 
+ * @return 
+ * 
+ * @example
+ *  //This function is not intended to be called explicitly
  */
+
 function onOpen() {
     console.log("*** METHOD_ENTRY: " + arguments.callee.name);
     
-    var accessToken = PropertiesService.getScriptProperties().getProperty(CONST_ACCESS_TOKEN_PROPERTY_NAME);
-    var refreshToken = PropertiesService.getScriptProperties().getProperty(CONST_REFRESH_TOKEN_PROPERTY_NAME);
-  
-    console.log("*** VARIABLE: accessToken: " + accessToken);
-    console.log("*** VARIABLE: refreshToken: " + refreshToken);
-  
-    if (isScriptPropertySet(CONST_ACCESS_TOKEN_PROPERTY_NAME) && isScriptPropertySet(CONST_REFRESH_TOKEN_PROPERTY_NAME)) {
-      //regenerate access token if the application was previously authorized but a session expired
-      console.log("*** INFO: " + "regenerating access token");
-      //regenerateToken(refreshToken);
-    } else {
-      //do nothing if the application was never authorized before
-      console.log("*** INFO: " + "NOT regenerating access token");
-    }
-  
     buildMenu();
   
     console.log("*** METHOD_EXIT: " + arguments.callee.name);
